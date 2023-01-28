67c041c34781360a4945bada17a9cbf843e0ca1c
Anton Kalinin <bratohalo@gmail.com>
2023-01-28 11:18:12 +0300

    Added Flyweight pattern

---
+++ src/main/java/org/edu/hse/seminar9/flyweight/FlyweightMain.java
@@ -0,0 +1,18 @@
+package org.edu.hse.seminar9.flyweight;
+
+import org.edu.hse.seminar9.flyweight.model.Imaginable;
+import org.edu.hse.seminar9.flyweight.router.CachableRouter;
+import org.edu.hse.seminar9.flyweight.router.Router;
+
+public class FlyweightMain {
+
+    public static void main(String[] args) {
+        final Router<Imaginable> imaginableRouter = new CachableRouter();
+
+        imaginableRouter.getData("profile://data");
+        imaginableRouter.getData("news://data");
+
+        imaginableRouter.getData("profile://data");
+        imaginableRouter.getData("profile://data2");
+    }
+}
---
+++ src/main/java/org/edu/hse/seminar9/flyweight/router/CachableRouter.java
@@ -0,0 +1,40 @@
+package org.edu.hse.seminar9.flyweight.router;
+
+import org.edu.hse.seminar9.flyweight.model.Imaginable;
+import org.edu.hse.seminar9.flyweight.model.News;
+import org.edu.hse.seminar9.flyweight.model.Profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CachableRouter implements Router<Imaginable> {
+
+    private static final Map<String, Imaginable> cache = new HashMap<>();
+
+    @Override
+    public Imaginable getData(String url) {
+        final Imaginable imaginable = cache.get(url);
+
+        if (imaginable != null) {
+            System.out.println("Got data from cache by url " + url);
+            return imaginable;
+        }
+
+        final Imaginable newImaginable;
+        if (url.contains("profile")) {
+            newImaginable = Profile.builder()
+                    .iconUrl("Profile Icon")
+                    .imageUrl("Profile Icon")
+                    .build();
+        } else {
+            newImaginable = News.builder()
+                    .newsImageUrl("News Image")
+                    .build();
+        }
+
+        System.out.println("Added data in cache by url " + url);
+        cache.put(url, newImaginable);
+
+        return newImaginable;
+    }
+}
