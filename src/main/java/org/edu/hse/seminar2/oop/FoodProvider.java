78577a6c89efe647d2b6b0ff2a289b1efb6d42c8
Anton Kalinin <aikalinin99@gmail.com>
2023-01-30 14:15:27 +0300

    Minor fixes, added seminar number in practice task and reverted seminar 2

---
+++ src/main/java/org/edu/hse/seminar2/oop/FoodProvider.java
@@ -0,0 +1,26 @@
+package org.edu.hse.seminar2.oop;
+
+import java.util.List;
+
+public class FoodProvider {
+
+    private FoodProvider() {
+
+    }
+    public static String makeCoffee(List<String> additives) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Coffee with ");
+        additives.forEach(item -> stringBuilder.append(item).append(" and "));
+        stringBuilder.append("java");
+
+        return stringBuilder.toString();
+    }
+
+    public static String makeBurger(List<String> additives) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\nBurger:\n");
+        additives.forEach(item -> stringBuilder.append(item).append("\n"));
+
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/org/edu/hse/seminar2/oop/Human.java
@@ -0,0 +1,53 @@
+package org.edu.hse.seminar2.oop;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public abstract class Human {
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+            .ofPattern("dd LLLL yyyy")
+            .withLocale(Locale.ENGLISH);
+
+    public abstract void speak(String text);
+
+    public abstract void walk(Double meters);
+
+    public abstract void eat(String[] food);
+
+    public abstract int getAge();
+
+    protected Human(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public final String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public final void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("My name is %s\nI was born in %s, I'm %d years old.",
+                name,
+                dateTimeFormatter.format(birthDate),
+                getAge());
+    }
+}
